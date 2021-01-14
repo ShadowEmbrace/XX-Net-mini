fee23b83f7366c858f1c017c43188307422ff93f
miketwes <mt.kongtong@gmail.com>
2021-01-14 21:08:58 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,17 @@
 @echo off
-%~d0
-cd %~dp0
-cd .\code\default\launcher
-python start.py
+@set PATH=%PATH%;C:\Program Files\OpenSSL-Win64\bin
+echo ****************************************
+echo XX-Net-mini
+echo ****************************************
+echo.
+echo.
+ver
+wmic computersystem get systemtype
+echo.
+title XX-Net-mini
+echo.
+openssl version
+python -V
+echo.
+cd %~dp0 
+cd .\code\default\launcher && python start.py
